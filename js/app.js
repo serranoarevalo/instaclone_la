ae9b59807af0cdd86fafca36aca7afe9f1453b32
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-07-16 15:39:12 +0900

    for justin ❤️

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// var weaponsList = ["AK-47", "Gun", "Machete", "Knife"];
+// var countries = ["Korea", "Japan", "Greece", "Colombia"];
+//
+// var soldier = {
+//   name: "Nicolas",
+//   weapon: weaponsList[2],
+//   country: countries[3],
+//   isDead: false,
+//   shoot: function() {
+//     console.log("Pew pew pew");
+//   },
+//   sayHi: function(sargent) {
+//     console.log("Hello " + sargent + " my name is " + this.name);
+//   }
+// };
+// function nameOfTheFunction(arguments){
+//   instructions
+// }
+//
+// function multiply(chicken, potato) {
+//   return chicken * potato;
+// }
+//
+// function division(lala, lolo) {
+//   return lala / lolo;
+// }
+// var age = prompt("How old are you?");
+//
+// if (age < 18) {
+//   console.log("You can't drink");
+// } else if (age > 18 && age < 45) {
+//   console.log("You're an adult");
+// } else {
+//   console.log("You are getting old");
+// }
+//
+// true && true = true
+// false && true = false
+// true && false = false
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+
+$(document).ready(function() {
+  var $heart = $(".heart");
+  $heart.click(function() {
+    $(this).toggleClass("fa-heart-o");
+    $(this).toggleClass("fa-heart heart-click");
+  });
+});
